2b0bb86f3059a7c372759ab2622a692a1e7bfe29
khalil1702 <khalil.chekili@esprit.tn>
2025-10-22 14:59:33 +0100

    page home

--- app_nutrition/lib/Screens/home_screen.dart
+++ app_nutrition/lib/Screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:translator/translator.dart';
-import '../main.dart'; // pour accéder à MyHomePage
+import '../main.dart'; 
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
